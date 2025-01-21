2676080a4fd06ec3f6d4d8a4f6630788c1f9de47
Ian <ianmugi580@gmail.com>
2025-01-21 09:35:06 +0300

    corrected code

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const form=document.getElementById('reservation').addEventListener('submit', function (event) {
-    event.preventDefault(); alert('Your Information Has Been Received Successfully!');
+document.getElementById('reservation').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Your Information Has Been Received Successfully!');
+    alert('You will receive a confirmation message shortly!');
 });
-
-const form2=document.getElementById('reservation').addEventListener('submit', function (event) {
-    event.preventDefault(); alert('You will receive a confirmation message shortly!');
-});
-
-
